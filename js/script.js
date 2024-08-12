9648219c401b32a67deb3ce994f425be568ce8e7
hyobin215 <dlgyqls215@naver.com>
2024-08-12 16:29:39 +0900

    20240812 4일차

--- js/script.js
+++ js/script.js
@@ -8,9 +8,16 @@ const swiper1 = new Swiper('#main_view', {
     // Optional parameters
     wrapperClass:'slider',
     slideClass:'view',
+    autoplay: { //자동슬라이드
+      delay: 5000, //하나의 슬라이드가 보여주는 시간
+    },
+    speed:800, //슬라이드가 전환되는(움직이는) 시간
+    spaceBetween:10, //슬라이드 사이 간격
   
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+      el: '.pager', //버튼영역 대상
+      bulletActiveClass:'active', //활성화된 요소에 추가되는 클래스
+      clickable:true, //클릭 시 슬라이드 이동
     },
   });
